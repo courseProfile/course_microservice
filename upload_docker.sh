8f3e3865b8fdbbc72915d6d35e922cbb52177933
courseProfile <samusARAN360@hotmail.com>
2020-02-02 12:55:05 +0100

    update sh files

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=courseudacity123/microservices:latest
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login --username=courseudacity123
+docker tag microservices:latest $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
